9cd5e3bc011897621c1e9cb888210cc78dfa8805
sugandhab31 <sugandhabansal31@gmail.com>
2025-02-03 17:50:16 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import './components-global/navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <navbar/>        
+      </header>
+    </div>
+  );
+}
+
+export default App;
